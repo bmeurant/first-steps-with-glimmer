877f3e54ca4367134c60f756967cab0936aed654
Baptiste Meurant <baptiste@meurant.io>
2017-04-16 23:15:14 +0000

    Actions & Immutability

--- src/ui/components/comics-library/component.ts
+++ src/ui/components/comics-library/component.ts
@@ -1,11 +1,15 @@
-import Component from "@glimmer/component";
+import Component, {tracked} from "@glimmer/component";
 
 export default class ComicsLibrary extends Component {
 
-  comics = [
+  @tracked comics = [
     {id: 1, title: "Akira", scriptwriter: "Katsuhiro Otomo", illustrator: "Katsuhiro Otomo"},
     {id: 2, title: "BlackSad", scriptwriter: "Juan Diaz Canales", illustrator: "Juanjo Guarnido"}, 
     {id: 3, title: "Calvin and Hobbes", scriptwriter: "Bill Watterson", illustrator: "Bill Watterson"}
   ];
+
+  deleteComic(comic) {
+    this.comics = this.comics.filter(x => x.id != comic.id);
+  }
 
 }
