70cfef64be0c1d8c8008f9b9c37cb41afe7720d8
Baptiste Meurant <baptiste@meurant.io>
2017-04-16 22:10:05 +0000

    Basic display

--- src/ui/components/comics-library/component.ts
+++ src/ui/components/comics-library/component.ts
@@ -2,4 +2,10 @@ import Component from "@glimmer/component";
 
 export default class ComicsLibrary extends Component {
 
+  comics = [
+    {id: 1, title: "Akira", scriptwriter: "Katsuhiro Otomo", illustrator: "Katsuhiro Otomo"},
+    {id: 2, title: "BlackSad", scriptwriter: "Juan Diaz Canales", illustrator: "Juanjo Guarnido"}, 
+    {id: 3, title: "Calvin and Hobbes", scriptwriter: "Bill Watterson", illustrator: "Bill Watterson"}
+  ];
+
 }
